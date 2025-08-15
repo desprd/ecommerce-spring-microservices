46ded1fcfac4b99b3a9d2082ca30c1396639cd8e
Ilya-Kanstanchuk <ilyakonst95@gmail.com>
2025-08-15 14:59:20 +0200

    Fix schema.sql

--- server/catalog/src/main/java/com/ilyaproject/catalog/controller/rest/CourseExternalController.java
+++ server/catalog/src/main/java/com/ilyaproject/catalog/controller/rest/CourseExternalController.java
@@ -8,6 +8,7 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,4 +27,6 @@ public class CourseExternalController {
         List<CourseFullDTO> courses = courseService.fetchAllCourses();
         return ResponseEntity.status(HttpStatus.OK).body(courses);
     }
+
+
 }
---
+++ server/catalog/src/main/resources/schema.sql
@@ -0,0 +1,21 @@
+CREATE TABLE IF NOT EXISTS author (
+    author_id BIGSERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    about TEXT,
+    created_at TIMESTAMP WITH TIME ZONE NOT NULL,
+    created_by varchar(20) NOT NULL,
+    updated_at TIMESTAMP WITH TIME ZONE DEFAULT NULL,
+    updated_by varchar(20) DEFAULT NULL
+);
+
+CREATE TABLE IF NOT EXISTS course (
+    course_id BIGSERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    description TEXT,
+    price NUMERIC(12, 2) NOT NULL,
+    author_id BIGINT NOT NULL REFERENCES author(author_id),
+    created_at TIMESTAMP WITH TIME ZONE NOT NULL,
+    created_by varchar(20) NOT NULL,
+    updated_at TIMESTAMP WITH TIME ZONE DEFAULT NULL,
+    updated_by varchar(20) DEFAULT NULL
+);
