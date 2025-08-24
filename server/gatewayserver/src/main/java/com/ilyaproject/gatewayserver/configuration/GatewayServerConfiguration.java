d1916d42f85629888e1739af2a8f1eb9683f44da
Ilya-Kanstanchuk <ilyakonst95@gmail.com>
2025-08-25 01:42:11 +0200

    Add retry pattern for every end point in gateway server

--- server/gatewayserver/src/main/java/com/ilyaproject/gatewayserver/configuration/GatewayServerConfiguration.java
+++ server/gatewayserver/src/main/java/com/ilyaproject/gatewayserver/configuration/GatewayServerConfiguration.java
@@ -3,16 +3,24 @@ package com.ilyaproject.gatewayserver.configuration;
 import com.ilyaproject.gatewayserver.service.RouteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.cloud.gateway.filter.ratelimit.RedisRateLimiter;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import reactor.core.publisher.Mono;
+
+import java.time.Duration;
 
 @Configuration
 public class GatewayServerConfiguration {
 
     @Autowired
     private RouteService routeService;
+
+
 
     @Bean
     public RouteLocator routes(RouteLocatorBuilder builder){
@@ -21,37 +29,65 @@ public class GatewayServerConfiguration {
                         .filters(f->
                                 f.circuitBreaker(config ->
                                         config.setName("coursesCircuitBreaker")
-                                              .setFallbackUri("forward:/coursesCache")))
+                                              .setFallbackUri("forward:/coursesCache"))
+                                .retry(retryConfig -> retryConfig.setRetries(3)
+                                        .setMethods(HttpMethod.GET)
+                                        .setBackoff(Duration.ofMillis(100), Duration.ofMillis(1000), 2, true))
+                                        .requestRateLimiter(config -> config.setRateLimiter(redisRateLimiter())
+                                                .setKeyResolver(userKeyResolver())))
                         .uri(routeService.getUriById("catalog")))
                 .route(r -> r.path("/api/rest/course/create")
                         .filters(f->
                                 f.circuitBreaker(config ->
                                         config.setName("createCoursesCircuitBreaker")
-                                                .setFallbackUri("forward:/contactSupport")))
+                                                .setFallbackUri("forward:/contactSupport"))
+                                        .requestRateLimiter(config -> config.setRateLimiter(redisRateLimiter())
+                                                .setKeyResolver(userKeyResolver())))
                         .uri(routeService.getUriById("catalog")))
                 .route(r -> r.path("/api/rest/author/create")
                         .filters(f->
                                 f.circuitBreaker(config ->
                                         config.setName("createAuthorCircuitBreaker")
-                                                .setFallbackUri("forward:/contactSupport")))
+                                                .setFallbackUri("forward:/contactSupport"))
+                                        .requestRateLimiter(config -> config.setRateLimiter(redisRateLimiter())
+                                                .setKeyResolver(userKeyResolver())))
                         .uri(routeService.getUriById("catalog")))
                 .route(r -> r.path("/api/rest/order/create")
                         .filters(f->
                                 f.circuitBreaker(config ->
                                         config.setName("createOrderCircuitBreaker")
-                                                .setFallbackUri("forward:/contactSupport")))
+                                                .setFallbackUri("forward:/contactSupport"))
+                                        .requestRateLimiter(config -> config.setRateLimiter(redisRateLimiter())
+                                                .setKeyResolver(userKeyResolver())))
                         .uri(routeService.getUriById("order")))
                 .route(r -> r.path("/api/rest/order/get/{id}")
                         .filters(f->
                                 f.circuitBreaker(config ->
                                         config.setName("orderCircuitBreaker")
-                                                .setFallbackUri("forward:/contactSupport")))
+                                                .setFallbackUri("forward:/contactSupport"))
+                                .retry(retryConfig -> retryConfig.setRetries(3)
+                                        .setMethods(HttpMethod.GET)
+                                        .setBackoff(Duration.ofMillis(100), Duration.ofMillis(1000), 2, true))
+                                        .requestRateLimiter(config -> config.setRateLimiter(redisRateLimiter())
+                                                .setKeyResolver(userKeyResolver())))
                         .uri(routeService.getUriById("order")))
                 .route(r -> r.path("/api/rest/payment/**")
                         .filters(f->
                                 f.circuitBreaker(config ->
                                         config.setName("paymentCircuitBreaker")
-                                                .setFallbackUri("forward:/contactSupport")))
+                                                .setFallbackUri("forward:/contactSupport"))
+                                        .requestRateLimiter(config -> config.setRateLimiter(redisRateLimiter())
+                                                .setKeyResolver(userKeyResolver())))
                         .uri(routeService.getUriById("payment"))).build();
+    }
+
+    @Bean
+    public RedisRateLimiter redisRateLimiter(){
+        return new RedisRateLimiter(1, 1, 1);
+    }
+
+    @Bean
+    public KeyResolver userKeyResolver(){
+        return exchange -> Mono.just(exchange.getRequest().getRemoteAddress().getHostString());
     }
 }
